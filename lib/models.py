fa5007727443ea5e9f7996eda53ed819b8c2471a
phuocem201 <phuocem201@gmail.com>
2022-04-16 08:34:16 +0700

    start

--- lib/models.py
+++ lib/models.py
@@ -1,11 +1,10 @@
 from datetime import timezone
 from enum import unique
 from sqlalchemy.sql import func
-from flask_login import UserMixin
 from . import db
 
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(150),unique = True)
     password = db.Column(db.String(150))
